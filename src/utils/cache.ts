6f45a42910584cdddccfe7c577eae9e09c8eea54
Thek28 <116284027+Thek28@users.noreply.github.com>
2023-03-09 08:47:54 +0800

    Add files via upload

---
+++ src/utils/cache.ts
@@ -0,0 +1,16 @@
+export const setCustomKey = (key: string) => {
+  if (typeof window !== "undefined" && key) {
+    localStorage.setItem("custom-key", key);
+  }
+};
+export const getCustomKey = () => {
+  if (typeof window !== "undefined") {
+    return localStorage.getItem("custom-key") || "";
+  }
+  return "";
+};
+export const clearCustomKey = () => {
+  if (typeof window !== "undefined") {
+    localStorage.removeItem("custom-key");
+  }
+};
