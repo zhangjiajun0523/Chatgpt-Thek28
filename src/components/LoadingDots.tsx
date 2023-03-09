3e3021bb9b3c51b2abcb89356aedc4052820efdb
Thek28 <116284027+Thek28@users.noreply.github.com>
2023-03-09 08:49:43 +0800

    Add files via upload

---
+++ src/components/LoadingDots.tsx
@@ -0,0 +1,23 @@
+import "../styles/loading-dots.css";
+
+const LoadingDots = ({
+  color = "000",
+  style = "small",
+}: {
+  color?: string;
+  style?: string;
+}) => {
+  return (
+    <span class={style == "small" ? "loading2" : "loading"}>
+      <span class="bg-black" />
+      <span class="bg-black" />
+      <span class="bg-black" />
+    </span>
+  );
+};
+
+export default LoadingDots;
+
+LoadingDots.defaultProps = {
+  style: "small",
+};
